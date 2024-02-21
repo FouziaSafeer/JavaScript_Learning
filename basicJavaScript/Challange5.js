220301ee0e63cd0e0cf5d6aefdeacd7136188f79
FouziaSafeer <fouziasafeer8@gmail.com>
2024-02-21 20:01:50 +0000

    working on chanllange 5

---
+++ basicJavaScript/Challange5.js
@@ -0,0 +1,31 @@
+//5.a. Create a <script> element. Inside the <script>, create a variable called "name",
+// and save your name in this variable (as a string). 
+let name="Fouzia";
+//5.b. Continuing from 5a, display the message "My name is: ${name}" 
+//in the console (insert the 'name' variable created in 5a into this message).
+console.log(`My name is: ${name}`);
+//.c. At a restraurant, you order 1 coffee ($5),
+// 2 bagals ($3 each), and 1 soup ($9). 
+//Calculate the cost and save it in a variable called 'cost'. 
+let coffeePrice=5;
+let bagalPrice=3;
+let soupPrice=9;
+let cost;
+cost=coffeePrice+2*bagalPrice+ soupPrice;
+console.log(cost);
+//5.d. Continuing from 5c, display "cost of food: $${cost}" in the console.
+console.log(`cost of food: $${cost}`);
+//5.e. Let's say the restaurance charges a 10% tax. Using the 'cost' variable from 5c, 
+//calculate the tax (hint: multiply by 0.1), and save the result in a variable.
+let tax;
+tax=cost*.01;
+console.log("Tax:$"+tax);
+//5.f Continuing from 5e, display "Tax (10%): $${tax}" in the console. 
+console.log(`display "Tax (10%): $${tax}`);
+//5.g. Continuing from 5f, calculate the total (cost+tax), 
+//save it in a variable called "totalcost", 
+//and display the message "Total cost: $${totalcost}" in the console.
+let totalCost=cost+tax;
+console.log(`Total cost: $${totalCost}`);
+//5.h. In the cart quantity project, add 2 more buttons "+4" and "+5", 
+//which increases the quantity by 4 and 5 when you click them. Try using +=
